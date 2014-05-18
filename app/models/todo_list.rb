d30c229d8fa6e6dc97b982f64e891c699321510f
zena <e.nosazena@gmail.com>
2014-05-18 14:41:16 -0400

    added validations for description

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -2,4 +2,6 @@ class TodoList < ActiveRecord::Base
 	validates :title, presence: true
 	# to check the test made on create_spec.rb
 	validates :title, length: {minimum: 3}
+	validates :description, presence: true
+	validates :description, length: {minimum: 5}
 end
--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -53,4 +53,43 @@ describe "Creating todo lists" do
 		expect(page).to_not have_content("This is what I'm doing today.")
 
 	end
+
+	it "displays an error when the todo list has no Description" do
+		# this is starting from a new page and saying a error should happend when no Description
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "Shopping list"
+		fill_in "Description", with: ""
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("Shopping list")
+
+	end
+
+	it "displays an error when the todo list has short Description" do
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "Shopping list"
+		fill_in "Description", with: "food"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("Shopping list")
+
+	end
 end
