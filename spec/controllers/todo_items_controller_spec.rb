7035a21de413d0f178c655f6895b0d7dbb9f22f1
zena <e.nosazena@gmail.com>
2014-05-19 17:11:07 -0400

    updated css and other stuff

--- spec/controllers/todo_items_controller_spec.rb
+++ spec/controllers/todo_items_controller_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 describe TodoItemsController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
-  end
+  # describe "GET 'index'" do
+  #   it "returns http success" do
+  #     get 'index'
+  #     response.should be_success
+  #   end
+  # end
+  # #######dont need since we are testing elsewhere
 
 end
